44f70b622a5da131447d7e351aab2ded8d0c9ebe
Mauricio <koitoer@gmail.com>
2016-06-16 10:09:31 -0600

    [INIT] Adding initial files

---
+++ src/main/java/com/koitoer/java/java8/LaunchClass.java
@@ -0,0 +1,11 @@
+package com.koitoer.java.java8;
+
+/**
+ * Created by mauricio.mena on 16/06/2016.
+ */
+public class LaunchClass {
+
+    public static void main(String[] args) {
+        System.out.println("Just a test ");
+    }
+}
